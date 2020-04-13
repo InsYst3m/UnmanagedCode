eced3e8f02cefb6e302a5eeca2448a28f8832a13
InsYst3m <godlike.insyst3m@gmail.com>
2020-04-13 17:47:52 +0300

    implemented Task2

---
+++ PowerManagementCOM/IPowerManagementComService.cs
@@ -0,0 +1,19 @@
+﻿using PowerManagement.Configuration.PowerManagementStructures;
+using System.Runtime.InteropServices;
+
+namespace PowerManagementCOM
+{
+    [ComVisible(true)]
+    [Guid("1d608944-16d9-4949-a987-20a3424651a9")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IPowerManagementComService
+    {
+        SystemPowerInformation GetSystemPowerInformation();
+        SystemBatteryState GetSystemBatteryState();
+        int GetLastWakeTime();
+        int GetLastSleepTime();
+        bool DeleteHibernateFile();
+        bool ReserveHibernateFile();
+        void SuspendState(bool hibernate, bool force, bool wakeupEventsDisabled);
+    }
+}
---
+++ PowerManagementCOM/PowerManagementComService.cs
@@ -0,0 +1,55 @@
+﻿using PowerManagement;
+using PowerManagement.Configuration.PowerManagementStructures;
+using System;
+using System.Runtime.InteropServices;
+
+namespace PowerManagementCOM
+{
+    [ComVisible(true)]
+    [Guid("3d9921b2-36ec-42ff-b024-64c711fbf25b")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class PowerManagementComService : IPowerManagementComService
+    {
+        private PowerManagementService _service;
+
+        public PowerManagementComService()
+        {
+            _service = new PowerManagementService();
+        }
+
+        public SystemPowerInformation GetSystemPowerInformation()
+        {
+            return _service.GetSystemPowerInformation();
+        }
+
+        public SystemBatteryState GetSystemBatteryState()
+        {
+            return _service.GetSystemBatteryState();
+        }
+
+        public int GetLastWakeTime()
+        {
+            return _service.GetLastWakeTime();
+        }
+
+        public int GetLastSleepTime()
+        {
+            return _service.GetLastSleepTime();
+        }
+
+        public bool DeleteHibernateFile()
+        {
+            return _service.DeleteHibernateFile();
+        }
+
+        public bool ReserveHibernateFile()
+        {
+            return _service.ReserveHibernateFile();
+        }
+
+        public void SuspendState(bool hibernate, bool force, bool wakeupEventsDisabled)
+        {
+            _service.SuspendState(hibernate, force, wakeupEventsDisabled);
+        }
+    }
+}
